fac9871689d2b6d0fdc6e2c903800c613e5331a2
adalah-sh <adalah.sh00@gmail.com>
2023-03-29 22:46:32 +0200

    add controllers files closed #10

--- src/controllers/error.js
+++ src/controllers/error.js
@@ -1,7 +1,10 @@
-const clientError = () => {};
-const serverError = () => {};
+const path = require('path');
 
-module.exports = {
-  clientError,
-  serverError,
+const clientError = (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '..', '..', 'public', '404.html'));
 };
+const serverError = (req, res) => {
+  res.status(500).sendFile(path.join(__dirname, '..', '..', 'public', '500.html'));
+};
+
+module.exports = { clientError, serverError };
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
 const { clientError, serverError } = require('./error');
+const { getPokemonById } = require('./fetchData');
 
 const router = express.Router();
+
+router.use('/search/:name', getPokemonById);
 
 router.use(clientError);
 router.use(serverError);
